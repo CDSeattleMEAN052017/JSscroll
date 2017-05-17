776cfc2463939cb1be2bec70a9acb607679748b8
Michelle <mcline23@hotmail.com>
2017-05-16 17:35:21 -0700

    Pipes Assignment

---
+++ pipes/src/app/currencies/currencies.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-currencies',
+  templateUrl: './currencies.component.html',
+  styleUrls: ['./currencies.component.css']
+})
+export class CurrenciesComponent implements OnInit {
+		value: number
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  	this.value = 42.20
+  }
+
+}
+
